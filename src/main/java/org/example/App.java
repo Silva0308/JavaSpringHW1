603dd0891e97f055d185c2d8f10669fa3c0e6ba2
Silva0308 <v.travinina@gmail.com>
2024-01-08 21:56:08 +0300

    HW1 DONE

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,42 @@
 package org.example;
+import com.google.gson.Gson;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        Person person1 = new Person("Ivan", "Ivanov", 25);
+        Person person2 = new Person("Petr", "Petrov", 35);
+        lang3(person1, person2);
+        System.out.println("------------------------------------------------------------");
+        gson(person1);
+    }
+
+    public static void lang3(Person person1, Person person2) {
+        Person person3 = person1;
+        ObjectUtils objectUtils = new ObjectUtils();
+        System.out.println(objectUtils.equals(person1, person2));
+        System.out.println(objectUtils.equals(person1, person3));
+        System.out.println(objectUtils.hashCode(person1));
+        System.out.println(objectUtils.hashCode(person2));
+        System.out.println(objectUtils.hashCode(person3));
+        System.out.println(objectUtils.toString(person1));
+        System.out.println(objectUtils.toString(person2));
+        System.out.println(ToStringBuilder.reflectionToString(person1));
+        System.out.println(ToStringBuilder.reflectionToString(person1));
+    }
+
+    public static void gson(Person person) {
+        Gson gson = new Gson();
+        String json = gson.toJson(person);
+        System.out.println(json);
+        Person newPerson = gson.fromJson(json, Person.class);
+        System.out.println(ToStringBuilder.reflectionToString(newPerson));
     }
 }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,2 +1,12 @@
-package org.example;public class Person {
+package org.example;
+public class Person {
+    String firstName;
+    String lastName;
+    int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
 }
